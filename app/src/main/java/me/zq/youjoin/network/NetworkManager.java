7d1a071d01b4dff88cdc4416b8303a30a7a2074a
freedomzzq <freedomzzq@gmail.com>
2015-11-22 00:11:45 +0800

    add request userinfo network api

--- app/src/main/java/me/zq/youjoin/network/NetworkManager.java
+++ app/src/main/java/me/zq/youjoin/network/NetworkManager.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import me.zq.youjoin.YouJoinApplication;
 import me.zq.youjoin.model.ImageInfo;
 import me.zq.youjoin.model.UpdateUserInfoResult;
 import me.zq.youjoin.model.UserInfo;
@@ -20,6 +21,7 @@ import me.zq.youjoin.utils.Md5Utils;
 
 /**
  * YouJoin
+ * 网络管理类，封装网络操作接口
  * Created by ZQ on 2015/11/12.
  */
 public class NetworkManager {
@@ -30,26 +32,45 @@ public class NetworkManager {
     public static final String USERNAME = "user_name";
     public static final String PASSWORD = "user_password";
     public static final String EMAIL = "user_email";
-    public static final String BASE_API_URL = "http://192.168.0.103:8088/youjoin-server/controllers/";
-//    public static final String BASE_API_URL = "http://www.tekbroaden.com/youjoin-server/controllers/";
 
     /**
-     * 网络接口URL
+     * 服务器接口URL
      */
+//    public static final String BASE_API_URL = "http://192.168.0.103:8088/youjoin-server/controllers/";
+    public static final String BASE_API_URL = "http://www.tekbroaden.com/youjoin-server/controllers/";
     public static final String API_SIGN_IN = BASE_API_URL + "signin.php";
     public static final String API_SIGN_UP = BASE_API_URL + "signup.php";
     public static final String API_UPDATE_USERINFO = BASE_API_URL + "update_userinfo.php";
     public static final String API_SEND_TWEET = BASE_API_URL + "send_tweet.php";
+    public static final String API_REQUEST_USERINFO = BASE_API_URL + "request_userinfo.php";
 
     private static RequestQueue mRequestQueue ;
 
 
+    /**
+     * 发送动态接口
+     * @param listener ResponseListener
+     */
     public static void postSendTweet(ResponseListener listener){
 
     }
 
     /**
-     * 个人资料更新接口
+     * 个人资料请求（下载）接口
+     * @param listener ResponseListener
+     */
+    public static void postRequestUserInfo(ResponseListener listener){
+        Map<String, String> params = new HashMap<>();
+        params.put("user_id", YouJoinApplication.getCurrUser().getId());
+        Request request = new PostObjectRequest(
+                API_REQUEST_USERINFO,
+                params,new TypeToken<UserInfo>(){}.getType(),
+                listener);
+        NetworkManager.getRequestQueue().add(request);
+    }
+
+    /**
+     * 个人资料更新（上传）接口
      * @param userInfo 用户实体类
      * @param picPath 头像的本地路径
      * @param listener ResponseListener
